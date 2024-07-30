fbb177a7cc419061a25e373094a5aa3f301ae968
Aditi Mehra <86202078+ADITIMEHRA722@users.noreply.github.com>
2024-07-30 13:45:03 +0530

    Update

--- netflix_clone/vite.config.js
+++ netflix_clone/vite.config.js
@@ -1,28 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-// vite.config.js
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     timeout: 600000, // Increase the timeout if your platform allows
-//   },
-//   optimizeDeps: {
-//     include: ["@emotion/react", "@emotion/styled", "@mui/material"],
-//     exclude: ["some-heavy-lib"]
-//   }
-// });
-
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -44,4 +19,3 @@ export default defineConfig({
     exclude: []
   }
 });
-
